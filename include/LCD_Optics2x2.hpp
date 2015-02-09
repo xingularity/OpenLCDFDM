6cebb6e834f5ccde992ad9a8d11deb68a85abc35
zonghan.xie <icbm0926@gmail.com>
2015-02-09 12:31:30 +0800

    加上計算polarization

--- include/LCD_Optics2x2.hpp
+++ include/LCD_Optics2x2.hpp
@@ -25,6 +25,7 @@ namespace LCDOptics{
     using LCD::EigenC22;
     using LCD::EigenM22;
     using Eigen::Vector3d;
+    using POLARTRACE = std::vector<Eigen::Vector2d>
     ///Jones matrix is a 2x2 matrix.
     typedef EigenC22 JONESMAT;
 
@@ -36,8 +37,16 @@ namespace LCDOptics{
         Optical2X2OneLayerBase(){}
         ///calculate one jones matrix and return its average refractive index and incident angles
         virtual double calcJonesMatrix(JONESMAT& m, Angle& iang, double lambda, double lastn) = 0;
+        ///calculate one jones matrix and light polarization, return the average refractive index and incident angles
+        virtual double calcJonesMatrix(JONESMAT& m, POLARTRACE& lightPolar, Angle& iang, double lambda, double lastn) = 0;
         virtual void interpolateNKForLambdas(DOUBLEARRAY1D lambdas_) = 0;
     protected:
+        ///calculate polarizations based on input Jones matrix
+        void calculatePolarization(const JONESMAT& m, POLARTRACE& lightPolar){
+            Eigen::Vector2d polar = lightPolar.back();
+            polar = M*polar;
+            lightPolar.push_back(polar);
+        }
         ///thickness of this layer
         double d;
         ///material type of this layer
@@ -62,6 +71,8 @@ namespace LCDOptics{
         Optical2X2OneLayer(double thickness, NKData _nk);
         ///calculate one jones matrix and return its average refractive index and incident angles
         virtual double calcJonesMatrix(JONESMAT& m, Angle& iang, double lambda, double lastn);
+        ///calculate one jones matrix and light polarization, return the average refractive index and incident angles
+        virtual double calcJonesMatrix(JONESMAT& m, POLARTRACE& lightPolar, Angle& iang, double lambda, double lastn);
         virtual void interpolateNKForLambdas(DOUBLEARRAY1D lambdas_);
     private:
         ///find nk for corresponding lambda
@@ -108,6 +119,27 @@ namespace LCDOptics{
         return nAvg;
     }
 
+    double Optical2X2OneLayer<ISOType>::calcJonesMatrix(JONESMAT& M, POLARTRACE& lightPolar,
+    Angle& iang, double lambda, double lastn){
+        double ts=0.0, tp=0.0;
+        double nAvg = findNK(lambda).real();
+        const double& theta_i = std::get<0>(iang);
+        const double& phi = std::get<1>(iang);
+        //refraction angle
+        double theta_r = std::asin(lastn*sin(theta_i)/nAvg);
+        EigenM22 tMat;
+        tMat << 0,0,0,0;
+        ts=2.0*lastn*cos(theta_i)/(lastn*cos(theta_i)+nAvg*cos(theta_r));
+        tp=2.0*lastn*cos(theta_i)/(lastn*cos(theta_r)+nAvg*cos(theta_i));
+        tMat(0,0)=ts;tMat(1,1)=tp;
+        M=tMat*M;
+        //calculate light polarization
+        calculatePolarization(M, lightPolar);
+
+        std::get<0>(iang)=theta_r;
+        return nAvg;
+    }
+
     /**
     Optical2X2UniaxialLayer represents base class of one layer of uniaxial material when calculating 2X2 extended Jones matrix.<br/>
     <em>It calculates incident tstp matrix, but doesn't do output tstp matrix.</em><br/>
@@ -121,6 +153,8 @@ namespace LCDOptics{
         void resetDirectors(DIRVEC _in);
         ///calculate one jones matrix and return its average refractive index and incident angles
         virtual double calcJonesMatrix(JONESMAT& m, Angle& iang, double lambda, double lastn);
+        ///calculate one jones matrix and light polarization, return the average refractive index and incident angles
+        virtual double calcJonesMatrix(JONESMAT& m, POLARTRACE& lightPolar, Angle& iang, double lambda, double lastn);
         virtual void interpolateNKForLambdas(DOUBLEARRAY1D lambdas_);
     private:
         ///find nk for corresponding lambda. If find in map, then return. Otherwise, interpolate immediately
@@ -213,6 +247,93 @@ namespace LCDOptics{
             M(0,1)*=exp(COMPD(0.0,-1.0)*kez*ld);
             M(1,0)*=exp(COMPD(0.0,-1.0)*koz*ld);
             M(1,1)*=exp(COMPD(0.0,-1.0)*koz*ld);
+
+            if (i<layernum-1){
+                caxis_temp << axisVec(i+1)(0), axisVec(i+1)(1), axisVec(i+1)(2);
+                Vector3d o1=kr.cross(caxis_temp);o1.normalize();
+                Vector3d e1=o1.cross(kr);e1.normalize();
+                EigenM22 eo_trans;
+                eo_trans << e.dot(e1), o.dot(e1), e.dot(o1), o.dot(o1);
+                M=eo_trans*M;
+                o=o1;e=e1;
+            }
+        }
+        //calculate output D matrix
+        EigenM22 eo_sp;
+        eo_sp << e.dot(s), o.dot(s), e.dot(p), o.dot(p);
+        M=eo_sp*M;
+        std::get<0>(iang)=theta_r;
+        return nAvg;
+    }
+
+    double Optical2X2OneLayer<UniaxialType>::calcJonesMatrix(JONESMAT& m,
+    POLARTRACE& lightPolar, Angle& iang, double lambda, double lastn){
+        Eigen::Vector2d tempPolar = lightPolar.back();
+        NKoNKe nn = findNK(lambda);
+        const COMPD& no = std::get<0>(nn);
+        const COMPD& ne = std::get<1>(nn);
+        double nAvg = no.real()*2.0/3.0 + ne.real()/3.0;
+
+        const double& theta_i = std::get<0>(iang);
+        const double& phi = std::get<1>(iang);
+        double theta_r=asin(lastn*sin(theta_i)/nAvg);
+
+        //tangential part of k, reference to Dr. Pochi Yeh's book
+        double alpha=(2.0*M_PI/lambda)*lastn*sin(theta_i)*cos(phi);
+        double beta=(2.0*M_PI/lambda)*lastn*sin(theta_i)*sin(phi);
+
+        EigenM22 tMat; tMat << 0,0,0,0;
+        double ts=0.0, tp=0.0;
+        ts=2.0*lastn*cos(theta_i)/(lastn*cos(theta_i)+nAvg*cos(theta_r));
+        tp=2.0*lastn*cos(theta_i)/(lastn*cos(theta_r)+nAvg*cos(theta_i));
+        tMat(0,0)=ts;tMat(1,1)=tp;
+
+        //Multiply ts tp matrix. M is now going into the zero thickness imaginary layer
+        M=tMat*M;
+        //calculate light polarization
+        calculatePolarization(M, lightPolar);
+
+        Vector3d zz; zz <<0.0,0.0,1.0;
+        Vector3d kr; kr << sin(theta_r)*cos(phi), sin(theta_r)*sin(phi), cos(theta_r);
+        Vector3d s,p;
+        if (kr==zz){
+            s << 0.0,-1.0,0.0;
+            p << 1.0,0.0,0.0;
+        }
+        else{
+            s=kr.cross(zz);s.normalize();
+            p=kr.cross(s);p.normalize();
+        }
+        Vector3d caxis_temp; caxis_temp << axisVec(0)(0), axisVec(0)(1), axisVec(0)(2);
+        Vector3d o=kr.cross(caxis_temp);o.normalize();
+        Vector3d e=o.cross(kr);e.normalize();
+        EigenM22 sp_eo;
+        sp_eo << s.dot(e), p.dot(e), s.dot(o), p.dot(o);
+        //convert sp coordinate to eo coordinate.
+        M=sp_eo*M;
+        double ld=d/layernum;
+        double angle[2]; //temp array for theta and phi of one axis
+        for (int i=0; i< layernum;i++){
+        //calcutate kez, koz
+            angle[0] = acos(axisVec(i)(2));
+            angle[1] = atan2(axisVec(i)(1), axisVec(i)(0));
+
+            double kd=alpha*cos(angle[1])+beta*sin(angle[1]);
+            double keb=alpha*sin(angle[1])*(-1.0)+beta*cos(angle[1]);
+            COMPD u=pow(sin(angle[0])/ne,2.0)+pow(cos(angle[0])/no,2.0);
+            COMPD v=kd*sin(2.0*angle[0])*(pow(1.0/ne,2.0)-pow(1.0/no,2.0));
+            COMPD w=(pow(kd*cos(angle[0]),2.0)+pow(keb,2.0))/pow(ne,2.0)
+                      +pow(kd*sin(angle[0])/no,2.0)
+                      -(2.0*M_PI/lambda)*(2.0*M_PI/lambda);
+            COMPD kez=(v+sqrt(v*v-4.0*u*w))/2.0/u;
+            COMPD koz=sqrt(no*no*(2.0*M_PI/lambda)*(2.0*M_PI/lambda)-alpha*alpha-beta*beta);
+
+            M(0,0)*=exp(COMPD(0.0,-1.0)*kez*ld);
+            M(0,1)*=exp(COMPD(0.0,-1.0)*kez*ld);
+            M(1,0)*=exp(COMPD(0.0,-1.0)*koz*ld);
+            M(1,1)*=exp(COMPD(0.0,-1.0)*koz*ld);
+            //calculate light polarization
+            calculatePolarization(M, lightPolar);
 
             if (i<layernum-1){
                 caxis_temp << axisVec(i+1)(0), axisVec(i+1)(1), axisVec(i+1)(2);

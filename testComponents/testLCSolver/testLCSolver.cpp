0755519185c1a145452531cd3de8a2fb79e53c46
zonghan.xie <icbm0926@gmail.com>
2015-04-10 14:08:39 +0800

    LC Solver test OK! Now add standard answers

--- testComponents/testLCSolver/testLCSolver.cpp
+++ testComponents/testLCSolver/testLCSolver.cpp
@@ -40,8 +40,8 @@ void writeDirectors(LCD1D::DIRVEC dirs, std::string suffix=""){
     std::fstream file;
     file.open(("Directors"+suffix+".txt").c_str(), std::ios::out|std::ios::trunc);
     for (int i = 0; i < dirs.extent(0); ++i){
-        file << dirs(i)(0) << " " << dirs(i)(1) << " " << dirs(i)(2) << " " << std::acos(dirs(i)(2))
-             << " " << std::atan2(dirs(i)(1), dirs(i)(0)) << " " << std::endl;
+        file << dirs(i)(0) << ", " << dirs(i)(1) << ", " << dirs(i)(2) << ", " << std::acos(dirs(i)(2))
+             << ", " << std::atan2(dirs(i)(1), dirs(i)(0)) << " " << std::endl;
     }
     file.flush();
     file.close();
@@ -69,13 +69,13 @@ void writeEFieldForLC(LCD::DOUBLEARRAY1D EFieldForLC, std::string suffix=""){
 
 void testTNCalculation(double dc_volt){
     std::cout << "Start TN Calculation without q0 under dc_volt=" + toString(dc_volt) << std::endl;
-    size_t layerNum = 48;
+    size_t layerNum = 40;
     double dt = 0.01; //ms
     size_t maxIter = 100000000;
     double max_error = 1.0e-8;
-    LCD1D::LCParamters lcParam = {4.8, 11.98, 3.36, 48.0, 11.5, 6, 14.3, 0.0};
-    LCD1D::DielecParameters tftpiParam = {0.1, 3.2};
-    LCD1D::DielecParameters cfpiParam = {0.1, 3.2};
+    LCD1D::LCParamters lcParam = {4.0, 12, 3.6, 60, 12.0, 6.5, 15.0, 0.0};
+    LCD1D::DielecParameters tftpiParam = {0.1, 3.5};
+    LCD1D::DielecParameters cfpiParam = {0.1, 3.5};
     LCD1D::RubbingCondition rubbing = {89.0*M_PI/180.0, 45.0*M_PI/180.0, 89.0*M_PI/180.0, 90.0*M_PI/180.0};
     LCD1D::Epsilon epsilonr(layerNum, lcParam.thick);
     epsilonr.setTFTPI(tftpiParam);
@@ -85,7 +85,7 @@ void testTNCalculation(double dc_volt){
     LCD1D::LCDirector lcDirector(layerNum, lcParam, rubbing, epsilonr);
     lcDirector.createVectorFormUpdater(potential, dt);
     double residual = std::numeric_limits<double>::max();
-    writeDirectors(lcDirector.getDirectors(), "_TNNoq0_"+toString(dc_volt)+"V_initialized_");
+    //writeDirectors(lcDirector.getDirectors(), "_TNNoq0_"+toString(dc_volt)+"V_initialized_");
     while(residual >= max_error){
         potential.update(dc_volt);
         residual = lcDirector.update();
@@ -96,8 +96,40 @@ void testTNCalculation(double dc_volt){
     writeDirectors(lcDirector.getDirectors(), "_TNNoq0_"+toString(dc_volt)+"V");
 }
 
+void testTNWithQ0Calculation(double dc_volt){
+    std::cout << "Start TN Calculation without q0 under dc_volt=" + toString(dc_volt) << std::endl;
+    size_t layerNum = 40;
+    double dt = 0.01; //ms
+    size_t maxIter = 100000000;
+    double max_error = 1.0e-8;
+    LCD1D::LCParamters lcParam = {4.0, 12, 3.6, 60, 12.0, 6.5, 15.0,  2.0*M_PI/70.0};
+    LCD1D::DielecParameters tftpiParam = {0.1, 3.5};
+    LCD1D::DielecParameters cfpiParam = {0.1, 3.5};
+    LCD1D::RubbingCondition rubbing = {89.0*M_PI/180.0, 45.0*M_PI/180.0, 89.0*M_PI/180.0, 90.0*M_PI/180.0};
+    LCD1D::Epsilon epsilonr(layerNum, lcParam.thick);
+    epsilonr.setTFTPI(tftpiParam);
+    epsilonr.setCFPI(cfpiParam);
+    LCD1D::Potential potential(layerNum+1, epsilonr, lcParam.thick/layerNum);
+    potential.createStaticUpdatePolicy();
+    LCD1D::LCDirector lcDirector(layerNum, lcParam, rubbing, epsilonr);
+    lcDirector.createVectorFormUpdater(potential, dt);
+    double residual = std::numeric_limits<double>::max();
+    //writeDirectors(lcDirector.getDirectors(), "_TNNoq0_"+toString(dc_volt)+"V_initialized_");
+    while(residual >= max_error){
+        potential.update(dc_volt);
+        residual = lcDirector.update();
+        std::cout << "residual = " << residual << std::endl;
+    };
+    writePotentials(potential.getPotentials(), "_TNq0_"+toString(dc_volt)+"V");
+    //writeEFieldForLC(potential.getEfieldForLC(), "_TNNoq0_"+toString(dc_volt)+"V");
+    writeDirectors(lcDirector.getDirectors(), "_TNq0_"+toString(dc_volt)+"V");
+}
+
 int main(int argc, const char *argv[])
 {
+    testTNCalculation(2.0);
     testTNCalculation(5.0);
+    testTNWithQ0Calculation(2.5);
+    testTNWithQ0Calculation(5.0);
     return 0;
 }

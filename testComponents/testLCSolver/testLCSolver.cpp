2b95dd3cee814a6186ef3c5f46cd1a70e6bcee78
zonghan.xie <icbm0926@gmail.com>
2015-04-10 14:47:34 +0800

    add dynamic step wave test case to LC solver tests.

--- testComponents/testLCSolver/testLCSolver.cpp
+++ testComponents/testLCSolver/testLCSolver.cpp
@@ -34,6 +34,7 @@
 #include <limits>
 #include <fstream>
 #include <cmath>
+#include <set>
 #include "LCD_UsefulFuncs.hpp"
 
 void writeDirectors(LCD1D::DIRVEC dirs, std::string suffix=""){
@@ -97,7 +98,7 @@ void testTNCalculation(double dc_volt){
 }
 
 void testTNWithQ0Calculation(double dc_volt){
-    std::cout << "Start TN Calculation without q0 under dc_volt=" + toString(dc_volt) << std::endl;
+    std::cout << "Start TN Calculation with q0 under dc_volt=" + toString(dc_volt) << std::endl;
     size_t layerNum = 40;
     double dt = 0.01; //ms
     size_t maxIter = 100000000;
@@ -125,11 +126,59 @@ void testTNWithQ0Calculation(double dc_volt){
     writeDirectors(lcDirector.getDirectors(), "_TNq0_"+toString(dc_volt)+"V");
 }
 
+void testTNWithQ0Dynamic(){
+    std::cout << "Start synamic TN Calculation with q0 and total time (ms) =" + toString(1000) << std::endl;
+    size_t layerNum = 40;
+    double dt = 0.01; //ms
+    size_t maxIter = 100000; // to 1000ms
+    LCD1D::LCParamters lcParam = {4.0, 12, 3.6, 60, 12.0, 6.5, 15.0,  2.0*M_PI/70.0};
+    LCD1D::DielecParameters tftpiParam = {0.1, 3.5};
+    LCD1D::DielecParameters cfpiParam = {0.1, 3.5};
+    LCD1D::RubbingCondition rubbing = {89.0*M_PI/180.0, 45.0*M_PI/180.0, 89.0*M_PI/180.0, 90.0*M_PI/180.0};
+    LCD1D::Epsilon epsilonr(layerNum, lcParam.thick);
+    epsilonr.setTFTPI(tftpiParam);
+    epsilonr.setCFPI(cfpiParam);
+    LCD1D::Potential potential(layerNum+1, epsilonr, lcParam.thick/layerNum);
+
+    //create step voltage profiles
+    std::map<double, double> profile; //ms, voltage
+    profile[0.0] = 2.5;
+    profile[200.0] = 5;
+    profile[400.0] = -2.5;
+    profile[600.0] = -5;
+    std::shared_ptr<LCD::WaveformBase> waveform;
+    waveform.reset(new LCD::StepWaveform(profile, 800.0));
+    potential.createDynamicUpdatePolicy(waveform);
+
+    LCD1D::LCDirector lcDirector(layerNum, lcParam, rubbing, epsilonr);
+    lcDirector.createVectorFormUpdater(potential, dt);
+    double residual = std::numeric_limits<double>::max();
+    size_t iterNum = 0;
+    //create dump time steps
+    std::set<size_t> dumpSteps;
+    dumpSteps.insert(19999); //just before 200ms
+    dumpSteps.insert(39999); //just before 400ms
+    dumpSteps.insert(59999); //just before 600ms
+    dumpSteps.insert(79999); //just before 800ms
+    dumpSteps.insert(99999); //just before 1000ms
+    while(iterNum <= maxIter){
+        potential.update(dt*iterNum);
+        lcDirector.update();
+        ++iterNum;
+        if (dumpSteps.find(iterNum) != dumpSteps.end()){
+            writePotentials(potential.getPotentials(), "_TNq0_"+toString(dt*iterNum)+"ms");
+            writeDirectors(lcDirector.getDirectors(), "_TNq0_"+toString(dt*iterNum)+"ms");
+        }
+    };
+}
+
+
 int main(int argc, const char *argv[])
 {
     testTNCalculation(2.0);
     testTNCalculation(5.0);
     testTNWithQ0Calculation(2.5);
     testTNWithQ0Calculation(5.0);
+    testTNWithQ0Dynamic();
     return 0;
 }
